0ca92246eb305c11335f3ba7c8349496dca383d1
dilane3 <komboudilane125@gmail.com>
2021-11-07 19:40:52 +0100

    adding types

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,12 @@
 "use strict";
 var btnElement = document.querySelector("#compteur");
 var count = 0;
-var increment = function (e) {
+function increment(e) {
+    e.preventDefault();
     count += 1;
-    var span = btnElement === null || btnElement === void 0 ? void 0 : btnElement.querySelector("span");
-    if (span)
+    if (btnElement instanceof HTMLButtonElement) {
+        var span = btnElement.querySelector("span");
         span.innerText = count.toString();
-};
-btnElement !== null && btnElement.addEventListener("click", increment);
+    }
+}
+btnElement.addEventListener("click", increment);
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
-const btnElement = document.querySelector("#compteur")
-let count = 0
+const btnElement = document.querySelector("#compteur") as HTMLButtonElement
+let count: number = 0
 
-const increment = (e: any) => {
+function increment(e: MouseEvent): void {
+    e.preventDefault()
+
     count += 1
 
-    const span = btnElement?.querySelector("span")
+    if (btnElement instanceof HTMLButtonElement) {
+        const span = <HTMLSpanElement>btnElement.querySelector("span")
 
-    if (span)
         span.innerText = count.toString()
+    }
 }
 
-btnElement !== null && btnElement.addEventListener("click", increment)
+btnElement.addEventListener("click", increment)
